051c9e02f52825b87b0def29a053d5bbf11f36c5
ashish <ashishjain1547@gmail.com>
2023-04-11 20:33:54 +0530

    #

---
+++ Ch 2 - Strings/2_strings_indexing and slicing.py
@@ -0,0 +1,23 @@
+# 1. Declare a string variable
+# 2. Print the third letter from that string
+# 3. Print the length of the string variable
+# 4. Reverse the string
+# 5. Check if a string is a palindrome.
+
+x = "jack smith" # Q1: Variable name, assignment operator, variable value
+#y = input("Please enter a string:")
+
+print(x[2])
+
+print(len(x))
+
+
+print(x[0:2])
+print(x[5:8])
+
+print(x[5:100]) # Python is intelligent about end index. If you give an end index larger than the last index, it automatically picks up the last index.
+
+
+
+
+
---
+++ Ch 3 - Operators/assignment_op.py
@@ -0,0 +1,10 @@
+# What is the output of this program?
+
+x = 5
+y = 10
+x = y
+
+
+print (x)
+print (y)
+
