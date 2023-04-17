7f86f3936213fcba4228dce2735f6ac29767cf39
ashish <ashishjain1547@gmail.com>
2023-04-17 21:45:44 +0530

    #

--- Ch 1- Calculator/calculator.py
+++ Ch 1- Calculator/calculator.py
@@ -1,21 +1,39 @@
-num1 = float(input("enter the 1st num "))
-num2 = float(input("enter the 2nd num "))
-which_type = input("enter which type of calculation you need ")
-if which_type == "add":
+# Takes three inputs
+
+num1 = float(input("enter the 1st num:")) # Variable declaration
+num2 = float(input("enter the 2nd num:"))
+
+which_op = input("enter which type of calculation you need:")
+
+print(which_op)
+print(type(which_op))
+
+print(num1)
+print(type(num1))
+
+# Built-in functions: input, print, float (to do type casting), type (get the type of the variable)
+
+if which_op == "add":
     print(num1 + num2)
-elif which_type == "subtract":
+
+elif which_op == "subtract":
     print(num1 - num2) 
-elif which_type == "multiply":
+
+elif which_op == "multiply":
     print(num1 * num2) 
 
-elif which_type == "divide":
+elif which_op == "divide":
+    # Preemptively checking for ZeroDivisionError.
+
     if num2 == 0:
         print("divisor can't be zero")
     else:
         print(num1 / num2)
-elif which_type == "power":
+
+elif which_op == "power":
     print(num1 ** num2)  
-elif which_type == "remainder":
+
+elif which_op == "remainder": # This is also called modulo.
     print(num1 % num2)          
 else:
     print("please enter a valid operation")
