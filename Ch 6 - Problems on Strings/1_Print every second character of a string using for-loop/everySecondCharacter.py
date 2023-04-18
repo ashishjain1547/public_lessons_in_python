56d7f57d0a334120eaa80a09a5587fb97621d40d
ashish <ashishjain1547@gmail.com>
2023-04-18 18:44:30 +0530

    #

---
+++ Ch 6 - Problems on Strings/1_Print every second character of a string using for-loop/everySecondCharacter.py
@@ -0,0 +1,27 @@
+name = "Johanth Yada" # Variable declaration
+
+# What a string is? A string is a list of characters that we can iterate.
+
+# We iterate over a sequence using the "in" keyword.
+
+# for i in name:
+#     print(i)
+
+# USING INTERATION OR FOR-LOOP
+cntr = 1 # This is because we need to pick every second character only. To keep a track of our characters in the string.
+for i in name:
+    if(cntr % 2 == 0):
+        print(i)
+    cntr += 1
+
+print("----------")
+# STRING INDEXING
+print(name[1 : len(name) : 2]) # start index (0), end index (length - 1), step (1)
+# Stings indexing starts from 0. And the first character we wanted was on index number "1".
+
+
+names = "Johanth Yada, Shaurya"
+print("----------")
+# STRING INDEXING
+print(names[5 : len(names) : 2]) # start index (0), end index (length - 1), step (1)
+# Stings indexing starts from 0. And the first character we wanted was on index number "1".
---
+++ Ch 6 - Problems on Strings/3_Check whether a given number is divisible by 7/script2 using divide and multiply.py
@@ -0,0 +1,6 @@
+n = 616
+quo = int(n/7)
+if quo*7 == n:
+    print("Divisible")
+else:
+    print("Not Divisible")
