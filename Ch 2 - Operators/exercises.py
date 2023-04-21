9925d61e490b468f24df818389cd1e7f054d3688
ashish <ashishjain1547@gmail.com>
2023-04-21 16:14:03 +0530

    #

--- Ch 2 - Operators/exercises.py
+++ Ch 2 - Operators/exercises.py
@@ -28,4 +28,15 @@ HINT:
 Step 1: declare a variable to take user input
 Step 2: declare a second variable Pi.
 Step 3: use the above two variables in formula to find the perimeter. 
+
+
+#4
+
+1. Take two numbers as input.
+2. Multiply both of them with 0.
+3. Show that they become equal.
+
+4. Divide each number by itself.
+5. Show again that they become equal.
 """
+
