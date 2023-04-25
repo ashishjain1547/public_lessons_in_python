5a18a353c127ef80d741bbf1e3a2b5a120a0433c
ashish <ashishjain1547@gmail.com>
2023-04-25 19:55:25 +0530

    #

--- Ch 2 - Variable Declaration and Operators/exercises.py
+++ Ch 2 - Variable Declaration and Operators/exercises.py
@@ -1,21 +1,18 @@
+"""
+
 # 1
 
-"""
 Declare two integer variables.
 
 And perform arithematic operations on them.
-"""
 
 # 2 
 
-"""
 Take two user inputs: dividend and divisor.
 Now perform division on them.
-"""
 
 #3
 
-"""
 Take input.
 Name: Radius
 
@@ -32,11 +29,24 @@ Step 3: use the above two variables in formula to find the perimeter.
 
 #4
 
-1. Take two numbers as input.
-2. Multiply both of them with 0.
-3. Show that they become equal.
+1. Take two integers as input.
+2. Show if they are equal or unequal.
 
-4. Divide each number by itself.
-5. Show again that they become equal.
+#5
+
+1. Take three inputs. Name them length, width and height
+2. Find the volume of the cuboid.
+
+
+3. Find the area of this cuboid.
+= 2*l*w + 2*w*h + 2*l*h
+
+#6
+1. Take an integer as input. Name it 'side'.
+2. Print the volume of the cube.
+3. Print the surface area of the cube.
+
+#7
+Find the area of the equilateral triangle.
 """
 
---
+++ Ch 2 - Variable Declaration and Operators/script.py
@@ -0,0 +1,8 @@
+# Variable declaration. r is holding the radius.
+
+r = input("Enter the radius: ") # input() gives you a string
+
+r = float(r) # Type casting builtin for getting the float value of r
+
+perimeter = 2 * 3.14 * r # Computation
+print(perimeter)
